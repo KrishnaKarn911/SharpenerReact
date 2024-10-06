641c1de34769614cd7f6ab0b01c1d8cbcbf44db1
KrishnaKarn911 <krishnakarn911@gmail.com>
2024-10-06 20:39:49 +0530

    props and multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
   return (
-    <div>
-      <li>Food - $5</li>
-      <li>Petrol - $20</li>
-      <li>Movie - $10</li>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__location">{props.location}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
+      </div>
     </div>
   );
 }
